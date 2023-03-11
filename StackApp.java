ffe69ed315aa25366d3a20ef9916e2a69c21e773
Thathsara Pramodya Thalangama <92014960+X-pose@users.noreply.github.com>
2023-03-11 13:40:22 +0530

    Create StackApp.java
    This is the main program. In here we create an object from the StackX class and perform stack-related operations on that object.

---
+++ StackApp.java
@@ -0,0 +1,27 @@
+package StackPack;
+
+public class StackApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StackX st = new StackX(5); //Creating an object from StackX class
+		
+    //Inserting values to the st object
+		st.push(90);
+		st.push(56);
+		st.push(78);
+		st.push(67);
+		st.push(45);
+		st.push(33);
+		st.push(125);
+		st.push(38);
+	
+    //Peeking the top value
+		System.out.println("Peek value is : - " + st.peek());
+		
+    //Removing values from top
+		System.out.println("Pop value is : - " + st.pop());
+    
+	}
+
+}
