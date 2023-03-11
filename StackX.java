9ffc5bce056aff0b750b7e3650551d431d459d81
Thathsara Pramodya Thalangama <92014960+X-pose@users.noreply.github.com>
2023-03-11 13:22:27 +0530

    Create StackX.java
    This is the StackX class. The stack-related operations are stored here. Objects of the StackX class can act as Stacks.

---
+++ StackX.java
@@ -0,0 +1,71 @@
+package StackPack;
+
+//Stack type - char
+public class StackX {
+	int top;
+	int maxSize;
+	char[] array;
+	
+  //Constructor
+	StackX(int max) {
+		maxSize = max;
+		top = -1;
+		array = new char[maxSize];
+		
+	}
+
+//Inserting values - Push method
+	public void Push(char value) {
+		if (!isFull()) {
+			array[++top] = value;
+		} else {
+			System.out.println("Stack is Full !!");
+		}
+		
+	}
+
+//Removing values from stack - Poping values
+	public char pop() {
+
+		if (!isEmpty()) {
+			return array[top--];
+		} else {
+			System.out.println("Stack is empty!!");
+			return 'B';
+		}
+	}
+
+//Peeking the top value
+	public char peek() {
+
+		if (!isEmpty()) {
+			return array[top];
+		} else {
+			System.out.println("Stack is empty!!");
+			return 'P';
+		}
+	}
+
+//Checking whether stack is empty or not
+	public boolean isEmpty() {
+		if (top == -1) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+//Checking whether stack is full or not
+	public boolean isFull() {
+		if (top == maxSize - 1) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+  
+	public void Display(){
+		System.out.println(array);
+	}
+}
